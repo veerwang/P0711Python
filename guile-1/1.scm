bf05e14e05ad9e9c3b424e222358855bcb67f882
kevin <kevin.wang2004@hotmail.com>
2013-09-17 15:48:00 +0800

    继续添加一些东西

--- guile-1/1.scm
+++ guile-1/1.scm
@@ -1,20 +1,47 @@
-#! /usr/bin/guile
+#! /usr/bin/guile -s
 !#
 
 ;this is my first guile program
 
 (define (f x)
- (if (< x 10) (+ x 12) (- x 12)))
+  (if (< x 10) (+ x 12) (- x 12)))
 
 (define (faci x)
- (if (zero? x) 1 (* x (faci (- x 1))) ))
+  (if (zero? x) 1 (* x (faci (- x 1))) ))
 
+(define company "Eddysun")
+(display company)
+(newline)
+(set! company "ABB")
+(display company)
+(newline)
+
+;procedure call
+(display (string-length (string-append "kevin" " love " "lingjian")))
+
+(newline)
+
+;my procedure
+(define myfun
+  (lambda (name addr) 
+	 (string-append "I love " name " live in " addr) )
+  )
+(display (myfun "lj" "xiamen"))
+
+;my second procedure
+(define (mynewfun name addr)
+  (string-append "I love " name " live in " addr) 
+  )
+ 
+(newline)
+(display (mynewfun "veer" "putian"))
+
+(newline)
 (begin (display "hello the world!")
-	(newline)
-	(display (f 8))
-	(newline)
-	(display (f 28))
-	(newline)
-	(display (faci 10))
-	(newline))
-
+       (newline)
+       (display (f 8))
+       (newline)
+       (display (f 28))
+       (newline)
+       (display (faci 10))
+       (newline))
