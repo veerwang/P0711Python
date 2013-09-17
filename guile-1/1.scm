39b6e46cf292d25bcd7cc37dde2b92e2d9708bdc
kevin <kevin.wang2004@hotmail.com>
2013-09-17 09:51:01 +0800

    添加guile文档

--- guile-1/1.scm
+++ guile-1/1.scm
@@ -6,12 +6,15 @@
 (define (f x)
  (if (< x 10) (+ x 12) (- x 12)))
 
+(define (faci x)
+ (if (zero? x) 1 (* x (faci (- x 1))) ))
 
 (begin (display "hello the world!")
 	(newline)
 	(display (f 8))
 	(newline)
 	(display (f 28))
+	(newline)
+	(display (faci 10))
 	(newline))
-
 
