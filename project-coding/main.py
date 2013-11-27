b4ad4c7e5f5081eb78cbccbdbafdb085db614183
kevin <kevin.wang2004@hotmail.com>
2013-11-27 15:31:52 +0800

    添加project-code目录

---
+++ project-coding/main.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+#coding=utf-8
+
+"""
+在这个区域内部实现各种注解
+"""
+
+
+if __name__ == '__main__':   	 #相当于c中的主函数
+	print "这是一个测试文档" #输出一个utf-8格式的字符串
+
+k = raw_input("输入一个数值:")	 #输入一个数字
+print k
