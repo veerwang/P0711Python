41fd206a18c3a52ef57d2015602847cc853fe9c7
kevin <kevin.wang2004@hotmail.com>
2018-07-11 20:54:50 +0800

    修正class工程

--- project-class/main.py
+++ project-class/main.py
@@ -10,27 +10,20 @@ def Set_ID(self,id):
 class Baseclass:
 	def __init__(self,id):
 		self.m_ID = id 
-		print "Baseclass create!"
+		print("Baseclass create!")
 	def __del__(self):
-		print "Baseclass destroy!"
+		print("Baseclass destroy!")
 
 	def Showme(self):
-		print self.m_ID
+		print(self.m_ID)
 
 	SetID = Set_ID
 
-# main function
-print "python class practice"
-
-bc = Baseclass(67)
-bc.SetID(100)
-bc.Showme()
-
 class Driverclass(Baseclass):
 	def __del__(self):
-		print "Driverclass destroy!"
+		print("Driverclass destroy!")
 	def Show_Sum(self):
-		print sum(i for i in range(0,11))
+		print(sum(i for i in range(0,11)))
 
 	m_Money = 10000
 
@@ -39,15 +32,41 @@ class Driverclass(Baseclass):
 
 #private functions
 	def __Showme(self):
-		print self.__m_SpecialMoney
+		print(self.__m_SpecialMoney)
 
 	def Showpmoney(self):
 		self.__Showme()
 
 
-sc = Driverclass(20)
-sc.Showme()
+class Gun():
 
-print sc.m_Money
-print sc.Showpmoney()
-print sc.Show_Sum()
+    bullets = 1000          # some like static variable of C++
+
+    def __init__(self,value):
+        self.bullets = value;
+
+    def dadada(self):
+        print(self.bullets)
+
+def main():
+    # main function
+    print("python class practice")
+
+    bc = Baseclass(67)
+    bc.SetID(100)
+    bc.Showme()
+
+    sc = Driverclass(20)
+    sc.Showme()
+
+    print(sc.m_Money)
+    print(sc.Showpmoney())
+    print(sc.Show_Sum())
+
+    gun = Gun(5000)
+    gun.dadada();
+    print(Gun.bullets)      #be care there is something different
+    print(gun.bullets)
+
+if __name__ == '__main__':
+    main()
