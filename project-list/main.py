40791fe7c561de8b65b9a59d3947a77acc7752f9
kevin.wang <wwei@landicorp.com>
2019-08-14 14:12:24 +0800

    添加list的功能

---
+++ project-list/main.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+#coding=utf-8
+
+import tuple.tupleopt
+
+if __name__ == "__main__":
+    print("hello the world")
+    mylist = ['hello','the','world','happy','new','year']
+    listlen = len(mylist)
+    print(listlen)
+
+    tuple.tupleopt.tuple_function()
