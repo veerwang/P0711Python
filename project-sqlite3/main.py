cb2edf181c50e5f3a8ba3747b26f65e7f29414e9
kevin.wang <wwei@landicorp.com>
2019-08-26 16:22:22 +0800

    添加数据库测试用例

---
+++ project-sqlite3/main.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+#coding=utf-8
+
+import sqlite3
+
+def init_sqlite_database():
+    # 创建一个数据库
+    conn   = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+    cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+    cursor.execute('insert into user (id, name) values (\'1\', \'Kevin\')')
+    cursor.execute('select * from user where id=?', '1')
+    result = cursor.fetchall()
+    # 打印查询结果
+    print(result)
+
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    print("sqlite3 database test")
+    init_sqlite_database()
