5b8ba5bc7966f8792b9f437ccc3e9af1a129d3e1
kevin <kevin.wang2004@hotmail.com>
2019-09-14 10:53:23 +0800

    添加dict的实验代码

---
+++ project-dict/main.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+#coding=utf-8
+
+#
+# 描述:		工程测试		 
+# 创建人: 	kevin.wang
+# 创建日期:     2019年09月14日 
+# 版本:		1.0.0     
+
+import os
+
+if __name__ == '__main__':
+
+    mydict = {1:'kevin',2:'veer',3:'leo',4:'mike'}
+    print(mydict)
+    print('key:2 ->  ' + mydict[2])
+    print(mydict.keys())
+    print(mydict.values())
+
