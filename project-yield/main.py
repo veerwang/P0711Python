b7b8ad83f8e237d46fc4e4717bf58564006b69ab
kevin.wang <wwei@landicorp.com>
2019-09-17 16:36:32 +0800

    添加yiled功能

---
+++ project-yield/main.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+#coding=utf-8
+
+#
+# 描述:		工程测试		 
+# 创建人: 	kevin.wang
+# 创建日期:     2019年09月17日 
+# 版本:		1.0.0     
+
+import os
+
+def Fun_A():
+    for i in range(4):
+        yield i
+        print("i value: " + str(i))
+
+if __name__ == '__main__':
+    print("the programe is for yiled")
+    g = Fun_A()
+    print(next(g))
+    print(next(g))
+
