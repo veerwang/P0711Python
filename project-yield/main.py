7f6737d1db7eeffe17f9b2b4244820c9fd556e56
kevin.wang <wwei@landicorp.com>
2019-09-17 18:25:45 +0800

    修正代码

--- project-yield/main.py
+++ project-yield/main.py
@@ -2,21 +2,22 @@
 #coding=utf-8
 
 #
-# 描述:		工程测试		 
+# 描述:		工程测试
 # 创建人: 	kevin.wang
 # 创建日期:     2019年09月17日 
-# 版本:		1.0.0     
+# 版本:		1.0.0
 
 import os
 
 def Fun_A():
     for i in range(4):
-        yield i
-        print("i value: " + str(i))
+        result = yield i
+        print("i value: " + str(result))
 
 if __name__ == '__main__':
     print("the programe is for yiled")
     g = Fun_A()
     print(next(g))
     print(next(g))
-
+    g.send(100)
+    print(next(g))
