b68f803ef7ad19644ce0b6ae504d1e2a8f278b50
kevin.wang <wwei@landicorp.com>
2019-10-10 10:57:06 +0800

    结束异常的测试

--- project-exception/main.py
+++ project-exception/main.py
@@ -9,6 +9,32 @@
 
 import os
 
+# 自定义错误类型
+class KevinError(Exception):
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = 'audi'
+
+# 自定义错误类型的确认
+def raise_self_def_test():
+    try:
+        fhandle = open('README','w')
+        fhandle.write('hello the world\n')
+        # Exception 也可以自行发出
+        raise KevinError('Specail Exception','veer')
+        fhandle.close()
+    # 对于特定的异常可以通过 type(Exception) 来确认
+    except FileNotFoundError as fe:
+        print('File No Found')
+    # Exception是全部异常的基类
+    except Exception as e: 
+        print(type(e))
+        print(e.args)
+        print(e)
+        print('---')
+        print(e.expression)
+        print(e.message)
+
 # 进行文件操作异常实验
 def raise_test():
     try:
@@ -55,5 +81,6 @@ def exception_test():
 
 if __name__ == '__main__':
     print("hello the world")
-    #exception_test()
+    raise_self_def_test()
+    exception_test()
     raise_test()
