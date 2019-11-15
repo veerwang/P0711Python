629d1eb3a3453b41c7c0a35e5ad0ddf0c6d7c354
kevin.wang <wwei@landicorp.com>
2019-11-15 11:35:42 +0800

    添加数据库代码

--- project-sqlite3/main.py
+++ project-sqlite3/main.py
@@ -30,9 +30,17 @@ def project_test():
     result = cursor.fetchall()
     # 打印查询结果
     print(result)
-    cursor.execute("create table if not exists "+"kevin"+"excel (id varchar(20) primary key NOT NULL,idcode text NOT NULL,describe text NOT NULL,Abscissa text NOT NULL,Ordinate text NOT NULL)")
-    for i in range(len(result)):
-        c.execute('update '+'kevin'+'excel set idcode = ?,describe = ?,Abscissa=?,Ordinate=? where Ordinate = ?',(result[i]["idcode"],result[i]["describe"],result[i]["Abscissa"],result[i]["Ordinate"],result[i]["oldOrdinate"]))
+    cursor.execute("create table if not exists "+"veer"+"excel (id varchar(20) primary key NOT NULL,idcode text NOT NULL,describe text NOT NULL,Abscissa text NOT NULL,Ordinate text NOT NULL)")
+    length = len(result)
+    print(str(length))
+    try:
+        for i in range(length):
+            print("kevin")
+            #c.execute('update '+'veer'+'excel set idcode = ?,describe = ?,Abscissa=?,Ordinate=? where Ordinate = ?',(result[i][1],result[i][2],result[i][3],result[i][4],result[i][5]))
+            data = " 1,\'1\',\'happy\',\'world\',\'fast\' " 
+            c.execute('INSERT INTO '+'veerexcel VALUES(%s)' % data)
+    except Exception as e:
+        logging.info(e) 
     conn.commit()
     cursor.close()
     conn.close()
@@ -40,12 +48,48 @@ def project_test():
 def read_table():
     conn   = sqlite3.connect("test.db")
     cursor = conn.cursor()
-    cursor.execute('select * from kevinexcel')
+    cursor.execute('select * from veerexcel')
     result = cursor.fetchall()
     # 打印查询结果
     print(result)
 
     cursor.close()
+    conn.close()
+
+#### 数据库测试代码
+def database_operator_init(name,value):
+    # 创建一个数据库
+    conn   = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('create table veertable (id varchar(20) primary key NOT NULL,idcode text NOT NULL,describe text NOT NULL,Abscissa text NOT NULL,Ordinate text NOT NULL)')
+        cursor.execute('insert into ' + name + 'table (id, idcode, describe, Abscissa, Ordinate)' + ' values (%d, "%s","%s","%s","%s")' %(1,'kevin','test','file',value))
+        cursor.execute('insert into ' + name + 'table (id, idcode, describe, Abscissa, Ordinate) values ("2", "Veer","happy","16","5")')
+        cursor.execute('select * from ' + name + 'table')
+    except BaseException as e:
+        print('except...', e)
+    else:
+        result = cursor.fetchall()
+        # 打印查询结果
+        print(result)
+    finally:
+        cursor.close()
+        conn.commit()
+        conn.close()
+
+def database_operator_read(name):
+    conn   = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute('select * from ' + name + 'table')
+
+    result = cursor.fetchall()
+    # 打印查询结果
+    print("read table")
+    print(result)
+
+    cursor.close()
+    conn.commit()
     conn.close()
 
 if __name__ == '__main__':
@@ -55,7 +99,9 @@ if __name__ == '__main__':
     try:
         #init_sqlite_database()
         #project_test()
-        read_table()
+        #read_table()
+        database_operator_init('veer','world')
+        database_operator_read('veer')
     except Exception as e:
-        logging.info("kevin -->")
         logging.info(e) 
+        print(e)
