48537de462740586fd85b6c3f2bef0ebc2161f66
kevin.wang <wwei@landicorp.com>
2020-12-29 13:22:39 +0800

    添加真彩字符显示界面

---
+++ project-txt-terminal/main.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+#coding=utf-8
+
+#
+# 描述:		工程测试		 
+# 创建人: 	kevin.wang
+# 创建日期:     2019年09月30日 
+# 版本:		1.0.0     
+
+import os
+import pathlib
+
+def disp_txt(): 
+    print("\x1b[38;2;175;255;0mhello the world")
+
+if __name__ == '__main__':
+    disp_txt()
