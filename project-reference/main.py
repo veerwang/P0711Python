2e3426c18ec8ecae73d2b5e39bc381ef9f8e5a86
kevin.wang <kevin@wisbo.com>
2021-03-11 15:48:08 +0800

    添加传址的方式

--- project-reference/main.py
+++ project-reference/main.py
@@ -10,7 +10,20 @@
 
 import os
 
-def modify_info(obj):
+def modify_list_info(objlist):
+    """docstring for modify_list_info
+    Description: 如果是列表也是传地址的
+    Args:
+    Returns:
+    Raises:
+    """
+    print("in -->" + objlist[0])
+    print("in -->" + objlist[1])
+
+    objlist[0] = 'dog'
+    objlist[1] = 'pig'
+    
+def modify_dict_info(obj):
     """docstring for modify_
     Description: 如果是字典则是传地址的
     Args:
@@ -20,9 +33,15 @@ def modify_info(obj):
     print("in -->" + obj[1])
     obj[1] = 'wwei'
 
-
 if __name__ == '__main__':
     obj = {1:'kevin'}
     print("out -->" + obj[1])
-    modify_info(obj)
+    modify_dict_info(obj)
     print("out -->" + obj[1])
+    print('----------------------')
+    objlist = ['1','2','3','4']
+    print("out -->" + objlist[0])
+    print("out -->" + objlist[1])
+    modify_list_info(objlist)
+    print("out -->" + objlist[0])
+    print("out -->" + objlist[1])
