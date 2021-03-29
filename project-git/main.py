53c5cd09d587f0a844a51bf9f3ea1f9439030251
kevin.wang <kevin@wisbo.com>
2021-03-29 17:35:08 +0800

    添加git操作功能

---
+++ project-git/main.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# coding=utf-8
+
+"""
+ 描述:		工程测试
+ 创建人:	kevin.wang
+ 创建日期:	2021年03月29日
+ 版本:		1.0.0
+"""
+
+
+from dulwich.repo import Repo
+
+if __name__ == '__main__':
+    r = Repo('.')	
+    r.head()
+    c = r[r.head()]
+    print(c.message)
