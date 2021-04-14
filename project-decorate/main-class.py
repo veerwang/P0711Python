493e6b1993727add958fbfbd9cb55e21af17422f
kevin.wang <kevin@wisbo.com>
2021-04-14 17:09:00 +0800

    小的修正

--- project-decorate/main-class.py
+++ project-decorate/main-class.py
@@ -14,18 +14,28 @@ class Log():
     """
 
     def __init__(self, func):
+        print('init')
         self._func = func
 
     def __call__(self):
         print("decorate start")
-        self._func()
+        return self._func()
         print("decorate end")
 
 
 @Log
 def normalFun():
     print("this is a normal function")
+    return 20
+
+@Log
+def highFun():
+    print("this is a normal function")
+    return 30
 
 
 if __name__ == '__main__':
-    normalFun()
+    r = normalFun()
+    print(r)
+    n = highFun()
+    print(n)
