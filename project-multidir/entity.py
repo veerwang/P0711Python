e6a02ccf676cbfd4dcccce854715168fd4416cdf
kevin.wang <kevin@wisbo.com>
2021-06-02 09:50:44 +0800

    添加多层级目录

---
+++ project-multidir/entity.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+# coding=utf-8
+
+"""
+ 描述:		工程测试
+ 创建人:	kevin.wang
+ 创建日期:	2021年06月02日
+ 版本:		1.0.0
+"""
+
+
+class entity:
+    """description"""
+
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    def dispName(self):
+        print("baby")
---
+++ project-multidir/main.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# coding=utf-8
+
+"""
+ 描述:		工程测试
+ 创建人:	kevin.wang
+ 创建日期:	2021年06月02日
+ 版本:		1.0.0
+"""
+
+
+from utils.window import window
+
+
+if __name__ == '__main__':
+    print("hello the world")
+    w = window()
+    w.dispName()
