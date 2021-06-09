042e1f09c2e4601237a6f8684bffcd12235e7a55
kevin.wang <kevin@wisbo.com>
2021-06-09 17:22:41 +0800

    修正日历例子

---
+++ project-calendar/main.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+# coding=utf-8
+
+"""
+ 描述:		工程测试
+ 创建人:	kevin.wang
+ 创建日期:	2021年06月09日
+ 版本:		1.0.0
+"""
+
+
+import datetime
+import calendar
+
+
+if __name__ == '__main__':
+    # 星期一为第一天
+    calendar.setfirstweekday(0)
+
+    # calendar(year, w=2, l=1, d=3, n=6)
+    # w: 每个日期两个字符
+    # l: 每行显示几行
+    # d: 月与月的间隔
+    # n: 每行显示几个月
+    result = calendar.calendar(datetime.datetime.now().year, 2, 1, 3, 3)
+
+    strlist = result.split('\n')
+    for i in range(len(strlist)):
+        print(strlist[i])
