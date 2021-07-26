e4f4c80e56151df678ee0dae81eab6f765541427
kevin.wang <kevin@wisbo.com>
2021-07-26 10:14:26 +0800

    添加python调用C++模块的范例

---
+++ project-python-call-c++/main.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# coding=utf-8
+
+"""
+ 描述:		工程测试
+ 创建人:	kevin.wang
+ 创建日期:	2021年07月26日
+ 版本:		1.0.0
+"""
+
+
+import os
+from ctypes import *
+
+
+if __name__ == '__main__':
+    print("python call C++ library")
+    solib = cdll.LoadLibrary(os.getcwd()+ "/libutils.so.0.0")
+    version = solib.getVersion()
+    print(version)
---
+++ project-python-call-c++/utils.cc
@@ -0,0 +1,25 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  类的描述
+ *
+ *        Version:  1.0
+ *        Created:  2021年07月26日
+ *       Revision:  1
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang
+ *   Organization:
+ *
+ * =============================================================================
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+extern "C" {
+int getVersion() { return 100; }
+}
