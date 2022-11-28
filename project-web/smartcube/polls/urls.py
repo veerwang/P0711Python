2c20839d190d2d2493a62132f24fe65a09c62141
kevin <kevin.wang2004@hotmail.com>
2022-11-28 14:13:22 +0800

    进行进一步的hardcode去除工作

--- project-web/smartcube/polls/urls.py
+++ project-web/smartcube/polls/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+# 添加命名空间后，对应的index.hml文件也要进行修改
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
     # ex: /polls/5/
