9d7771bb2270b3bf55589d0168c36ae6c19307cc
kevin <kevin.wang2004@hotmail.com>
2022-11-28 11:15:26 +0800

    另外一种写法

--- project-web/smartcube/polls/views.py
+++ project-web/smartcube/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
@@ -7,11 +7,8 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
