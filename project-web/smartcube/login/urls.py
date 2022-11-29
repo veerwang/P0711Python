248972b0d11687b8decdd00348976e51309b3de6
kevin <kevin.wang2004@hotmail.com>
2022-11-29 14:08:48 +0800

    继续完善login应用的内容

---
+++ project-web/smartcube/login/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+# 添加命名空间后，对应的index.hml文件也要进行修改
+app_name = 'login'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- project-web/smartcube/smartcube/urls.py
+++ project-web/smartcube/smartcube/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('login/', include('login.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
