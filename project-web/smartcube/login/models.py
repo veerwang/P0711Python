26508134e21fe3a3fd8e9d01035c85e742a8501a
kevin <kevin.wang2004@hotmail.com>
2022-11-29 16:26:19 +0800

    继续完善代码

--- project-web/smartcube/login/models.py
+++ project-web/smartcube/login/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=11, default='13488508087')
 
     def __str__(self):
-        return self.first_name + self.last_name
+        return self.first_name + self.last_name + ':' + self.phone_number
