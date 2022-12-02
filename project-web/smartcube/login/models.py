e184fd4bc8c4cd2541d7bbe741110569d8af95c4
kevin <kevin.wang2004@hotmail.com>
2022-12-02 13:48:22 +0800

    添加用户数据结构

--- project-web/smartcube/login/models.py
+++ project-web/smartcube/login/models.py
@@ -10,3 +10,24 @@ class Person(models.Model):
 
     def __str__(self):
         return self.first_name + self.last_name + ':' + self.phone_number
+
+
+class User(models.Model):
+    """正式的用户数据，用于管理账户"""
+    # 登陆输入的用户名英文 xmadmin
+    Account = models.CharField(max_length=32, unique=True)
+    # 显示用户的名称，例如厦门水文站
+    Name = models.CharField(max_length=64, default=Account + '用户')
+    # 密码, Hash-256
+    Password = models.CharField(max_length=256)
+    # 权限 超级管理员,管理员，用户
+    Authority = models.IntegerField(default=0)
+    # 属地省
+    LocationProvince = models.CharField(max_length=48)
+    # 属地市
+    LocationCity = models.CharField(max_length=48)
+    # 属地Arae
+    LocationArea = models.CharField(max_length=48)
+
+    def __str__(self):
+        return self.Name
