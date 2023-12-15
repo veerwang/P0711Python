820cfb4a3bffbd9ab506308b79c6fec6df50c68a
kevin <kevin.wang2004@hotmail.com>
2023-12-15 15:42:13 +0800

    继续完善yield函数

--- project-yield/main.py
+++ project-yield/main.py
@@ -16,8 +16,15 @@ def Fun_A():
 
 if __name__ == '__main__':
     print("the programe is for yiled")
+    # 由于函数当中有yield关键字，不会被执行
+    # 只是将函数对象赋值给g
     g = Fun_A()
+    # next(g) 会执行到yield i 返回，即返回 0
+    # 即打印 0
     print(next(g))
+    # 继续执行result是none,没有内容
+    # 之后返回 1
     print(next(g))
+    # send(100)标示 将yield i 中的 i赋值为100
     g.send(100)
     print(next(g))
